cbb81de78bcf45c4c9ec7a204f616edc14dde71f
heartxploit <58259661+heartxploit@users.noreply.github.com>
2019-11-27 18:53:43 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,62 @@
+
+<html>
+<head>
+<title>HeartXploit | NULL SQU4D </title>
+<iframe width="0" height="0" scrolling="no" frameborder="no" src="http://www.youtube.com/embed/mM_ZsWsUWic?rel=0;&autoplay=1" frameborder="0" allowfullscreen></iframe>
+<meta property="og:image" content="https://i.ibb.co/m5Gm44v/20191127-182023.png"/>
+<link href="https://fonts.googleapis.com/css?family=Cuprum" rel="stylesheet">
+<link href="https://i.ibb.co/m5Gm44v/20191127-182023.png" rel="icon-x">
+<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+<meta name="title" content="HeartXploit"> 
+<meta name="description" content="HeartXploit Page"> 
+<link href="https://fonts.googleapis.com/css?family=Share+Tech+Mono" rel="stylesheet"> 
+</head> 
+<body style="color: #fff; margin:0;font: normal 14px/20px Share Tech Mono, Helvetica, sans-serif; height:100%; background-color: #000000;"/>
+ <div style="height:auto; min-height:100%; "> 
+	<div style="text-align: center; width:800px; margin-left: -400px; position:absolute; top: 20%; left:50%;"> 
+	<h1 style="margin:0; font-size:180px; line-height:250px; font-weight:bold;"><img src="https://i.ibb.co/m5Gm44v/20191127-182023.png" height="500" width="500"></h1> 
+	<h2 style="margin-top:20px;font-size: 50px;">HeartXploit</h2><p>- Senior Adminstrator - <p>- NULL SQU4D - <br><br>				<marquee width="300"><font color="white">I Don't Play With Heart,I Play With Cyber Security</font></marquee><br><br><br>☣️☢️Contact Me☣️☢️: <a href="https://m.facebook.com/profile.php?id=100011986661486">Facebook</a><br></div> </p>
+	<script>
+function rtclickcheck(keyp){
+  if (navigator.appName == "Netscape" && keyp.which == 3) {
+    alert(mymessage);
+    return false;
+  }
+
+  if (navigator.appVersion.indexOf("MSIE") != -1 && event.button == 2) {
+    alert("Fuck You Bitch"); 	
+    return false;
+  }
+}
+
+document.onmousedown = rtclickcheck
+</script>
+
+<body oncontextmenu="return false" onselectstart="return false" ondragstart="return false"></body>
+<script type="text/javascript">
+
+//form tags to omit in NS6+:
+var omitformtags=["input", "textarea", "select"]
+
+omitformtags=omitformtags.join("|")
+
+function disableselect(e){
+if (omitformtags.indexOf(e.target.tagName.toLowerCase())==-1)
+return false
+}
+
+function reEnable(){
+return true
+}
+
+if (typeof ********.onselectstart!="undefined")
+********.onselectstart=new Function ("return false")
+else{
+********.onmousedown=disableselect
+********.onmouseup=reEnable
+}
+
+</script>
+<body onkeydown="return false">
+</body>
+</html>
